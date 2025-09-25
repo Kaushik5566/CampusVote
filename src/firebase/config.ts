ccac3bb3b25b339ff52b1594e51935ff47bfa08a
Kaushik Maurya <kaushikmaurya8355@gmail.com>
2025-09-25 20:04:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-567530340-eefa7",
+  "appId": "1:789502206823:web:383542a8838e6e76b5c5c2",
+  "apiKey": "AIzaSyC8SUfPXCaaj9lYW5J2v_OhXeaaO3lgmw8",
+  "authDomain": "studio-567530340-eefa7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "789502206823"
+};
